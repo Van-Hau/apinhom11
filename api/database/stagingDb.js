70822766c7bfd3362c76fee6acdfe6ad9d8a0ff5
Nguyễn Văn Hậu <19130065@st.hcmuaf.edu.vn>
2022-11-17 17:40:35 +0700

    a

--- api/database/stagingDb.js
+++ api/database/stagingDb.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit: 10, //important
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "202.151.176.142",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASS || "EHNksa58813",
   database: process.env.DB_NAME || "staging",
   debug: false
 });
